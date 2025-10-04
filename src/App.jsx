b9f0bbc0898f7d6feb17d08fda4161be99bfab55
Carolyn Wilkinson <carolynwilkinson94@gmail.com>
2025-10-04 11:55:31 +0800

    made a to do list

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import TodoItem from "./Components/TodoItem";
+
+
+function App() {
+  
+
+  return (
+  <>
+    <h1>My To Do List</h1>
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+  </>
+  )
+    
+  
+}
+
+export default App
