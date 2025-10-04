4f5f3f1c870d6dee2c3473adbb4174effc3fdbab
Carolyn Wilkinson <carolynwilkinson94@gmail.com>
2025-10-04 12:37:27 +0800

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-
+import {useState} from 'react';
 import TodoItem from "./Components/TodoItem";
 
 
+
 function App() {
+  const [todos, setTodos] = useState([
+    "Do a thing", 
+    "Do another thing", 
+    "Build an app",
+  ]);
   
+
+
 
   return (
   <>
     <h1>My To Do List</h1>
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
+    {todos.map((todo, index) => (
+      <TodoItem text={todo} key={index} />
+    ))}
   </>
-  )
+  );
     
   
 }
